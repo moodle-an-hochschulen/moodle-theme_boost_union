0ac47604d6b5f2283774924dd56eb04c359b969f
Alexander Bias <alexander.bias@lernlink.de>
2022-04-29 15:34:34 +0200

    Allow full Behat runs with boost_union fixes #14.

--- CHANGES.md
+++ CHANGES.md
@@ -6,6 +6,8 @@ Changes
 
 ### Unreleased
 
+=======
+* 2022-06-20 - Allow full Behat runs with Boost Campus suite.
 * 2022-06-20 - Fill README.md, helps to resolve #3.
 * 2022-04-29 - Adopt all changes which have happened in Boost core for the Moodle 4.0 release
 * 2022-03-17 - Create boilerplate as Boost child theme without any additional features.
---
+++ tests/behat/behat_theme_boost_union_behat_repository_upload.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Behat upload repository-related step definition overrides for the Boost Union theme.
+ *
+ * @package    theme_boost_union
+ * @category   test
+ * @copyright  2022 Luca Bösch, BFH Bern University of Applied Sciences luca.boesch@bfh.ch
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
+
+require_once(__DIR__ . '/../../../../repository/upload/tests/behat/behat_repository_upload.php');
+
+use Behat\Mink\Exception\ExpectationException as ExpectationException;
+
+/**
+ * Repository upload-related step definition overrides for the Boost Union theme.
+ *
+ * @package    theme_boost_union
+ * @category   test
+ * @copyright  2022 Luca Bösch, BFH Bern University of Applied Sciences luca.boesch@bfh.ch
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_theme_boost_union_behat_repository_upload extends behat_repository_upload {
+}
