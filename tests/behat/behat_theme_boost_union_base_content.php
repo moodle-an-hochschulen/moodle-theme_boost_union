0cc3bde38c1a413b73c0ec0be995f637951feca6
Andre Menrath <andre.menrath@uni-graz.at>
2023-02-13 11:44:12 +0100

    Add behat tests for the customisation of the appearance of H5P activities

--- tests/behat/behat_theme_boost_union_base_content.php
+++ tests/behat/behat_theme_boost_union_base_content.php
@@ -112,4 +112,69 @@ class behat_theme_boost_union_base_content extends behat_base {
     public function i_am_on_page3_page() {
         $this->execute('behat_general::i_visit', ['/theme/boost_union/pages/page3.php']);
     }
+
+    /**
+     * Convert a rem CSS value to the computed px value.
+     *
+     * @copyright  2023 University of Graz
+     * @author     André Menrath <andre.menrath@uni-graz.at>
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private function rem_css_value_to_px($value) {
+        // Extract the rem float value.
+        if (preg_match("/([\d\.]+)\s*rem/", $value, $matches) ) {
+            $remvalue = floatval($matches[1]);
+            // Javascript query to get the font size for the html root.
+            $javascript = "window.getComputedStyle(document.querySelector('html')).getPropertyValue('font-size')";
+            $rem = $this->getSession()->evaluateScript($javascript);
+            // Remove the "px" to get the value.
+            $rem = floatval(substr($rem, 0, -2));
+            // Compute the rem value to px.
+            $value = $rem * $remvalue;
+            return strval($value) . 'px';
+        }
+    }
+
+    /**
+     * Check for the computed value of a CSS property of a given element.
+     *
+     * @copyright  2023 University of Graz
+     * @author     André Menrath <andre.menrath@uni-graz.at>
+     *
+     * @Then the element :element should have a :property of :value
+     *
+     * @param string $element
+     * @param string $property
+     * @param string $value
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function check_computed_style_of_element($element, $property, $value) {
+        if (!$this->has_tag('javascript')) {
+            throw new DriverException('The file picker is only available with javascript enabled');
+        }
+        // Check whether the value is given in the unit "rem".
+        if (str_ends_with($value, 'rem')) {
+            $value = $this->rem_css_value_to_px($value);
+        }
+
+        // Javascript code to get the value of a CSS property of a given element.
+        $javascript = "window.getComputedStyle(document.querySelector('$element')).getPropertyValue('$property')";
+
+        $computedvalue = $this->getSession()->evaluateScript($javascript);
+
+        if ($computedvalue !== $value) {
+            throw new \Exception(sprintf(
+                'Expected the value of "%s" for the property "%s" to be "%s", but got "%s"',
+                $element,
+                $property,
+                $value,
+                $computedvalue
+            ));
+        }
+    }
 }
--- tests/behat/theme_boost_union_looksettings_h5p.feature
+++ tests/behat/theme_boost_union_looksettings_h5p.feature
@@ -17,13 +17,36 @@ Feature: Configuring the theme_boost_union plugin for the "H5P" tab on the "Look
       | teacher1 | C1     | editingteacher |
       | student1 | C1     | student        |
 
-  # Unfortunately, this can't be tested with Behat yet
-  # And as this feature file for this tab can't be empty, we just add a dummy step.
+  @javascript
   Scenario: Setting: Raw CSS for H5P - Add custom SCSS to a mod_h5p content type
     When I log in as "admin"
+    And I navigate to "Appearance > Themes > Boost Union > Look" in site administration
+    And I click on "H5P" "link" in the "#adminsettings .nav-tabs" "css_element"
+    # Ajust the border radius in a h5p accordeon to fit the look and feel of modern moodle.
+    And I set the field "Raw CSS for H5P" to multiline:
+    """
+    .h5p-accordion {
+        border-radius: .5rem !important;
+    }
+    """
+    And I press "Save changes"
+    And I log in as "teacher1"
+    And I am on "Course 1" course homepage with editing mode on
+    Given the following "activity" exists:
+      | activity        | h5pactivity                   |
+      | course          | C1                            |
+      | name            | H5P package           |
+      | packagefilepath | h5p/tests/fixtures/ipsums.h5p |
+    When I am on the "H5P package" "h5pactivity activity" page
+    And I wait until the page is ready
+    And I switch to "h5p-player" class iframe
+    And I switch to "h5p-iframe" class iframe
+    Then the element ".h5p-accordion" should have a "border-top-left-radius" of ".5rem"
 
-  # Unfortunately, this can't be tested with Behat yet
+  # Unfortunately, this can't be tested with Behat yet, cause the mod_hvp plugin only works when
+  # php warnings don't trigger the shutdown handler. See https://github.com/h5p/moodle-mod_hvp/issues/487.
   # And as this feature file for this tab can't be empty, we just add a dummy step.
+  @javascript
   Scenario: Setting: Raw CSS for H5P - Add custom SCSS to a mod_hvp content type
     When I log in as "admin"
 
