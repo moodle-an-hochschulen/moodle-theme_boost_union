8aad644e89763adc2785fc72407fea741d63f891
Diphylla <annika-lambert@t-online.de>
2023-03-17 16:28:43 +0000

    Added frontpage slider functionality

--- lib.php
+++ lib.php
@@ -50,6 +50,10 @@ define('THEME_BOOST_UNION_SETTING_ADVERTISEMENTTILES_COLUMN_COUNT', 4);
 define('THEME_BOOST_UNION_SETTING_ADVERTISEMENTTILES_FRONTPAGEPOSITION_BEFORE', 1);
 define('THEME_BOOST_UNION_SETTING_ADVERTISEMENTTILES_FRONTPAGEPOSITION_AFTER', 2);
 
+define('THEME_BOOST_UNION_SETTING_SLIDES_COUNT', 6);
+define('THEME_BOOST_UNION_SETTING_SLIDER_FRONTPAGEPOSITION_BEFORE', 1);
+define('THEME_BOOST_UNION_SETTING_SLIDER_FRONTPAGEPOSITION_AFTER', 2);
+
 define('THEME_BOOST_UNION_SETTING_HEIGHT_100PX', '100px');
 define('THEME_BOOST_UNION_SETTING_HEIGHT_150PX', '150px');
 define('THEME_BOOST_UNION_SETTING_HEIGHT_200PX', '200px');
@@ -308,6 +312,11 @@ function theme_boost_union_get_extra_scss($theme) {
     // Setting: Course overview block.
     $content .= theme_boost_union_get_scss_courseoverview_block($theme);
 
+    // Load slider css if slider setting is enabled.
+    if (get_config("theme_boost_union", "slideractivatedsetting")) {
+        $content .= theme_boost_union_get_slider_scss();
+    }
+
     return $content;
 }
 
@@ -405,7 +414,8 @@ function theme_boost_union_pluginfile($course, $cm, $context, $filearea, $args, 
         $filearea === 'loginbackgroundimage' || $filearea === 'additionalresources' ||
                 $filearea === 'customfonts' || $filearea === 'courseheaderimagefallback' ||
                 $filearea === 'touchiconsios' ||
-                preg_match("/tilebackgroundimage[2-9]|1[0-2]?/", $filearea))) {
+                preg_match("/tilebackgroundimage[2-9]|1[0-2]?/", $filearea) ||
+                preg_match("/sliderbackgroundimage[2-9]|1[0-2]?/", $filearea))) {
         $theme = theme_config::load('boost_union');
         // By default, theme files must be cache-able by both browsers and proxies.
         if (!array_key_exists('cacheability', $options)) {
