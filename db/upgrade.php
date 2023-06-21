59d3eb88ca516d4ffdf4675098544d4bb1f9ca88
Harshal Abak <abakharshal@gmail.com>
2023-06-21 21:28:24 +0530

    length change

--- db/upgrade.php
+++ db/upgrade.php
@@ -42,7 +42,7 @@ function xmldb_theme_boost_union_upgrade($oldversion) {
         $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
         $table->add_field('title', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null);
         $table->add_field('description', XMLDB_TYPE_TEXT, null, null, null, null, null);
-        $table->add_field('description_format', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
+        $table->add_field('description_format', XMLDB_TYPE_INTEGER, '2', null, null, null, null);
         $table->add_field('sort', XMLDB_TYPE_INTEGER, '10', null, null, null, null);
         $table->add_field('applytocohorts', XMLDB_TYPE_INTEGER, '1', null, null, null, null);
         $table->add_field('applytocohorts_ids', XMLDB_TYPE_TEXT, null, null, null, null, null);
