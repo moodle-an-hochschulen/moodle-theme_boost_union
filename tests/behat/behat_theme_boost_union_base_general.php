b0485c650ab2bc43bcb984bf87fae549f62dfa24
Alexander Bias <bias@alexanderbias.de>
2023-10-29 12:50:10 +0100

    Test: Add missing Behat tests for Scroll-spy implementation, solves #86.

--- tests/behat/behat_theme_boost_union_base_general.php
+++ tests/behat/behat_theme_boost_union_base_general.php
@@ -58,14 +58,12 @@ class behat_theme_boost_union_base_general extends behat_base {
     }
 
     /**
-     * Scroll the page.
+     * Scroll the page to a given coordinate.
      *
      * @copyright 2016 Shweta Sharma on https://stackoverflow.com/a/39613869.
      * @Then /^I scroll page to x "(?P<posx_number>\d+)" y "(?P<posy_number>\d+)"$/
-     *
      * @param string $posx The x coordinate to scroll to.
      * @param string $posy The y coordinate to scroll to.
-     *
      * @return void
      * @throws Exception
      */
@@ -74,6 +72,93 @@ class behat_theme_boost_union_base_general extends behat_base {
             $this->getSession()->executeScript("(function(){document.getElementById('page').scrollTo($posx, $posy);})();");
         } catch (Exception $e) {
             throw new \Exception("Scrolling the page to given coordinates failed");
+        }
+    }
+
+    /**
+     * Scroll the page to the DOM element with the given ID.
+     *
+     * @copyright 2023 Alexander Bias <bias@alexanderbias.de>
+     * @Then I scroll page to DOM element with ID :arg1
+     * @param string $selector
+     * @return void
+     * @throws Exception
+     */
+    public function i_scroll_page_to_dom_element_with_id($selector) {
+        $scrolljs = "(function(){
+                let element = document.getElementById('$selector');
+                let y = element.offsetTop;
+                document.getElementById('page').scrollTo(0, y);
+        })();";
+        try {
+            $this->getSession()->executeScript($scrolljs);
+        } catch (Exception $e) {
+            throw new \Exception('Scrolling the page to the \''.$selector.'\' DOM element failed');
+        }
+    }
+
+    /**
+     * Checks if the top of the page is at the top of the viewport.
+     *
+     * @copyright 2023 Alexander Bias <bias@alexanderbias.de>
+     * @Then page top is at the top of the viewport
+     * @throws ExpectationException
+     */
+    public function page_top_is_at_top_of_viewport() {
+        $posviewportjs = "
+            return (
+                document.getElementById('page').scrollTop
+            )
+        ";
+        $positionviewport = $this->evaluate_script($posviewportjs);
+        if ($positionviewport != 0) {
+            throw new ExpectationException('The page top is not at the top of the viewport', $this->getSession());
+        }
+    }
+
+    /**
+     * Checks if the top of the page is not at the top of the viewport.
+     *
+     * @copyright 2023 Alexander Bias <bias@alexanderbias.de>
+     * @Then page top is not at the top of the viewport
+     * @throws ExpectationException
+     */
+    public function page_top_is_not_at_top_of_viewport() {
+        $posviewportjs = "
+            return (
+                document.getElementById('page').scrollTop
+            )
+        ";
+        $positionviewport = $this->evaluate_script($posviewportjs);
+        if ($positionviewport == 0) {
+            throw new ExpectationException('The page top is at the top of the viewport', $this->getSession());
+        }
+    }
+
+    /**
+     * Checks if the given DOM element is at the top of the viewport.
+     *
+     * @copyright 2023 Alexander Bias <bias@alexanderbias.de>
+     * @Then DOM element :arg1 is at the top of the viewport
+     * @param string $selector
+     * @throws ExpectationException
+     */
+    public function dom_element_is_at_top_of_viewport($selector) {
+        $poselementjs = "
+            return (
+                document.getElementById('$selector').offsetTop
+            )
+        ";
+        $positionelement = $this->evaluate_script($poselementjs);
+        $posviewportjs = "
+            return (
+                document.getElementById('page').scrollTop
+            )
+        ";
+        $positionviewport = $this->evaluate_script($posviewportjs);
+        if ($positionelement > $positionviewport + 50 ||
+                $positionelement < $positionviewport - 50) { // Allow some deviation of 50px of the scrolling position.
+            throw new ExpectationException('The DOM element \''.$selector.'\' is not a the top of the page', $this->getSession());
         }
     }
 }
