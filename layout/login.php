59d25c855354faaf5f065ba9d7be3245b2926372
Luca Bösch <luca.boesch@bfh.ch>
2023-11-05 21:18:35 +0100

    Improvement: Add the option to suppress the footer button, solves #444.

--- layout/login.php
+++ layout/login.php
@@ -53,6 +53,9 @@ require_once(__DIR__ . '/includes/footnote.php');
 // Include the template content for the static pages.
 require_once(__DIR__ . '/includes/staticpages.php');
 
+// Include the template content for the footer button.
+require_once(__DIR__ . '/includes/footer.php');
+
 // Include the template content for the info banners.
 require_once(__DIR__ . '/includes/infobanners.php');
 
