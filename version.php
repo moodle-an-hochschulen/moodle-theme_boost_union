584394566220cf99ccd35ce19cf0303cc2016fe8
Alexander Bias <alexander.bias@lernlink.de>
2023-11-12 18:52:31 +0100

    Release v4.3-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023102002;
-$plugin->release = 'v4.3-r1';
+$plugin->version = 2023102003;
+$plugin->release = 'v4.3-r2';
 $plugin->requires = 2023100900;
 $plugin->supported = [403, 403];
 $plugin->maturity = MATURITY_STABLE;
