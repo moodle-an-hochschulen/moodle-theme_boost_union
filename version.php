7a9ccfa25bed5da2fa7e7485d38d3ef53e83fc39
Alexander Bias <bias@alexanderbias.de>
2023-12-16 21:48:27 +0100

    Improvement: Configurable sort order in menu items of smart menu, solves #403.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023010532;
+$plugin->version = 2023010533;
 $plugin->release = 'v4.1-r16';
 $plugin->requires = 2022112805.04;
 $plugin->supported = [401, 401];
