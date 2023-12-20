aea588b980c423ae75d9f195b282d2aa9204a4f7
Alexander Bias <bias@alexanderbias.de>
2023-12-20 20:29:40 +0100

    Review changes

--- db/caches.php
+++ db/caches.php
@@ -61,6 +61,14 @@ $definitions = [
                     'theme_boost_union_cohort_deleted',
                 ],
         ],
+        // This cache stores the touch icon files for iOS (which are uploaded in the Boost Union settings)
+        // to avoid that the files have to be read from the filearea on every page load.
+        'touchiconsios' => [
+            'mode' => cache_store::MODE_APPLICATION,
+            'simplekeys' => true,
+            'simpledata' => true,
+            'staticacceleration' => true,
+        ],
         // This cache stores the smart menus.
         'smartmenus' => [
                 'mode' => cache_store::MODE_APPLICATION,
