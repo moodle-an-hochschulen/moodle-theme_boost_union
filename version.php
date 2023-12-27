8020eeb980b0ccb8462a674c57f33a9801ceac4b
Alexander Bias <bias@alexanderbias.de>
2023-12-27 12:27:52 +0100

    Improvement: Add content style setting to the advertisement tiles, solves #519.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023010536;
+$plugin->version = 2023010537;
 $plugin->release = 'v4.1-r16';
 $plugin->requires = 2022112805.04;
 $plugin->supported = [401, 401];
