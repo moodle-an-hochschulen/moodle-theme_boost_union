3e9fb19bad38f9bb4b78832029dd24efe9f6be42
Alexander Bias <bias@alexanderbias.de>
2024-03-13 15:41:34 +0100

    Refine favourites popover contributed in #289

--- CHANGES.md
+++ CHANGES.md
@@ -7,7 +7,7 @@ Changes
 ### Unreleased
 
 * 2024-03-13 - Bugfix: Custom course fields of type "Textarea" were not conditionally hidden in the smart menu configuration, resolves #576.
-* 2024-03-01 - Improvement: implement new setting to show starred courses in a popover menu in navbar, solves #289.
+* 2024-03-01 - Feature: Show starred courses popover in the navbar, resolves #289.
 
 ### v4.2-r11
 
--- lib.php
+++ lib.php
@@ -609,6 +609,17 @@ function theme_boost_union_output_fragment_icons_list($args) {
  * @return string
  */
 function theme_boost_union_render_navbar_output() {
-    require_once(__DIR__ . '/locallib.php');
-    return theme_boost_union_get_favourites_popover_menu();
+    global $CFG;
+
+    // Require local library.
+    require_once($CFG->dirroot . '/theme/boost_union/locallib.php');
+
+    // Initialize the navbar content.
+    $content = '';
+
+    // Setting: Show starred courses popover in the navbar.
+    $content .= theme_boost_union_get_navbar_starredcoursespopover();
+
+    // Return.
+    return $content;
 }
