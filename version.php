aa27dab02e83dd397f6ebf45a42acf3aa00b2233
Alexander Bias <bias@alexanderbias.de>
2024-03-18 20:13:54 +0100

    Review changes

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023090132;
+$plugin->version = 2023090133;
 $plugin->release = 'v4.2-r12';
 $plugin->requires = 2023042403;
 $plugin->supported = [402, 402];
