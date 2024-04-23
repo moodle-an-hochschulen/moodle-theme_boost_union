17f6e895b7971157c516bbb311196e833a78a4f2
Alexander Bias <alexander.bias@lernlink.de>
2024-04-23 20:40:03 +0200

    Release v4.2-r15

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023090137;
-$plugin->release = 'v4.2-r14';
+$plugin->version = 2023090138;
+$plugin->release = 'v4.2-r15';
 $plugin->requires = 2023042403;
 $plugin->supported = [402, 402];
 $plugin->maturity = MATURITY_STABLE;
