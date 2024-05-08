dd091a0980a5b160bbdd9e08ae3d831a903bc109
Christian Wolters <christianwolters@users.noreply.github.com>
2024-05-08 06:50:09 +0200

    Improvement: Add navigation to policy overview page, resolves #633 (#642)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023090138;
+$plugin->version = 2023090139;
 $plugin->release = 'v4.2-r15';
 $plugin->requires = 2023042403;
 $plugin->supported = [402, 402];
