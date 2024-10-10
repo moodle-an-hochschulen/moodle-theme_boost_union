ccdf53a46fe9d57ddda3b4eff0ddddb979ff823c
Alexander Bias <abias@ssystems.de>
2024-10-10 08:26:39 +0200

    Release v4.4-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024060100;
-$plugin->release = 'v4.4-r1';
+$plugin->version = 2024060101;
+$plugin->release = 'v4.4-r2';
 $plugin->requires = 2024042200;
 $plugin->supported = [404, 404];
 $plugin->maturity = MATURITY_STABLE;
