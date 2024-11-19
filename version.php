9083cb5357b4f7f218322e56630d8b94385f0866
Nikolai Jahreis <63058688+NJahreis@users.noreply.github.com>
2024-11-19 07:12:30 +0100

    Improvement: Add restriction for site admins to smartmenu(item), resolves #421 . (#656)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024060104;
+$plugin->version = 2024060105;
 $plugin->release = 'v4.4-r5';
 $plugin->requires = 2024042200;
 $plugin->supported = [404, 404];
