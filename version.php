62757a44d34c96416a74041ad4918f98a9a77c25
Alexander Bias <abias@ssystems.de>
2025-02-05 08:36:39 +0100

    Improvement: Hide the 'Menu item mode' settings for smart menu items which are not of the 'dynamic courses' item type, resolves #804. (#805)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024100711;
+$plugin->version = 2024100712;
 $plugin->release = 'v4.5-r5';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
