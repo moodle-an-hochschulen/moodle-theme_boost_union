9c15d24a33276da0d0ed749c7f8fad1a314075e7
Alexander Bias <bias@alexanderbias.de>
2025-02-07 13:02:57 +0100

    Release v4.3-r22

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023102056;
-$plugin->release = 'v4.3-r21';
+$plugin->version = 2023102057;
+$plugin->release = 'v4.3-r22';
 $plugin->requires = 2023100900;
 $plugin->supported = [403, 403];
 $plugin->maturity = MATURITY_STABLE;
