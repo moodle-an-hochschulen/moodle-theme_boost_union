a151f05e92f086d6a6b97e3eee4acbac92c93498
Alexander Bias <bias@alexanderbias.de>
2025-03-11 07:18:55 +0100

    Release v4.1-r38

--- CHANGES.md
+++ CHANGES.md
@@ -4,7 +4,7 @@ moodle-theme_boost_union
 Changes
 -------
 
-### Unreleased
+### v4.1-r38
 
 * 2025-03-09 - Improvement: Introduce a Boost Union specific string for "submit" to be used in the accessbility support form which allows better translatons, resolves #830.
 
--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2023010583;
-$plugin->release = 'v4.1-r37';
+$plugin->version = 2023010584;
+$plugin->release = 'v4.1-r38';
 $plugin->requires = 2022112806;
 $plugin->supported = [401, 401];
 $plugin->maturity = MATURITY_STABLE;
