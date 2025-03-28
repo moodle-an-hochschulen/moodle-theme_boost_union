aa6d0b0fbe1f1390c05095ea80bb3bc5df6d0a80
Alexander Bias <abias@ssystems.de>
2025-03-28 21:33:31 +0100

    Improvement: Add resizing to flavour logo and compact logo, resolves #212.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024100724;
+$plugin->version = 2024100725;
 $plugin->release = 'v4.5-r11';
 $plugin->requires = 2024100702;
 $plugin->supported = [405, 405];
