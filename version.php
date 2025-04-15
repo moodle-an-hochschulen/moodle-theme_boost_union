c386c875dc39f507bcc0f5840d9c0eba40cfdac2
Alexander Bias <abias@ssystems.de>
2025-04-15 15:30:42 +0200

    Upgrade: Remove mediumwidth setting, resolves #780

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024100728;
+$plugin->version = 2025041400;
 $plugin->release = 'v4.5-r13';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
