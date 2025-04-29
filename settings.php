802e5ee3798152d70c8eb57ed840787bab66ac74
Alexander Bias <abias@ssystems.de>
2025-04-29 18:54:15 +0200

    Upgrade: Remove recentlyaccesseditemstintenabled setting as the icons in this block are tinted by default now in Moodle 5.0.

--- settings.php
+++ settings.php
@@ -1065,20 +1065,6 @@ if ($hassiteconfig || has_capability('theme/boost_union:configure', context_syst
         $setting->set_updatedcallback('theme_reset_all_caches');
         $tab->add($setting);
 
-        // Create Recently accessed items block heading.
-        $name = 'theme_boost_union/recentlyaccesseditemsheading';
-        $title = get_string('recentlyaccesseditemsheading', 'theme_boost_union', null, true);
-        $setting = new admin_setting_heading($name, $title, null);
-        $tab->add($setting);
-
-        // Setting: Tint activity icons in the recently accessed items block.
-        $name = 'theme_boost_union/recentlyaccesseditemstintenabled';
-        $title = get_string('recentlyaccesseditemstintenabled', 'theme_boost_union', null, true);
-        $description = get_string('recentlyaccesseditemstintenabled_desc', 'theme_boost_union', null, true);
-        $setting = new admin_setting_configselect($name, $title, $description, THEME_BOOST_UNION_SETTING_SELECT_NO, $yesnooption);
-        $setting->set_updatedcallback('theme_reset_all_caches');
-        $tab->add($setting);
-
         // Add tab to settings page.
         $page->add($tab);
 
