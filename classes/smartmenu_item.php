ffe51bf3b5f1f994e1f3117a8ec3109dd4d89098
prasanna-lmsace <prasanna_t@lmsace.com>
2025-05-07 13:46:53 +0530

    Bugfix: Smart menu: menu bar displayed although there are no items to display, resolves #849.

--- classes/smartmenu_item.php
+++ classes/smartmenu_item.php
@@ -1375,6 +1375,9 @@ class smartmenu_item {
             'sort' => uniqid(), // Support third level menu.
             'sortdata' => $sortdata,
             'imagealt' => $imagealt ?? $title,
+            'desktop' => $this->item->desktop,
+            'tablet' => $this->item->tablet,
+            'mobile' => $this->item->mobile,
         ];
 
         if ($haschildren && !empty($children)) {
