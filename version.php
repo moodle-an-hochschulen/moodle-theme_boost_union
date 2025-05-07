7d74dfa25e27f6fd1574dcb8b3b4abc8a644c228
prasanna-lmsace <prasanna_t@lmsace.com>
2025-05-07 13:46:53 +0530

    Bugfix: Smart menu: menu bar displayed although there are no items to display, resolves #849.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041407;
+$plugin->version = 2025041408;
 $plugin->release = 'v5.0-r3';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
