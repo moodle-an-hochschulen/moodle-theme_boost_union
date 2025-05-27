863068587260697bce39098ac80d865451ef5c2e
Alexander Bias <bias@alexanderbias.de>
2025-05-27 16:06:34 +0200

    Release v5.0-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041404;
-$plugin->release = 'v5.0-r1';
+$plugin->version = 2025041405;
+$plugin->release = 'v5.0-r2';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
 $plugin->maturity = MATURITY_STABLE;
