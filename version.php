10ffbf31e2a0bb6d3f81cea3fa5ce4c7fd10bc02
Alexander Bias <abias@ssystems.de>
2025-07-19 20:23:28 +0200

    Re-write the smart menu item value for 'no icon'

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024100746;
+$plugin->version = 2024100747;
 $plugin->release = 'v4.5-r21';
 $plugin->requires = 2024100702;
 $plugin->supported = [405, 405];
