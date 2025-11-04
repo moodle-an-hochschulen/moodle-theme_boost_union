2087b464ee536aff5ba479d63e486a25e59678a9
Alexander Bias <abias@ssystems.de>
2025-11-04 08:38:38 +0100

    Glitch: upgrade.php did not match install.xml regarding the theme_boost_union_snippets table, resolves #1062 (#1089)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2024100756;
+$plugin->version = 2024100757;
 $plugin->release = 'v4.5-r27';
 $plugin->requires = 2024100702;
 $plugin->supported = [405, 405];
