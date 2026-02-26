2dc60f6a212c4ad6d82c574a4d5a777b5e921353
berthob98 <65233148+berthob98@users.noreply.github.com>
2026-02-26 13:26:19 +0100

    Add new login layouts and other login settings, resolves #1055 (#1112)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041441;
+$plugin->version = 2025041442;
 $plugin->release = 'v5.0-r17';
 $plugin->requires = 2025041401;
 $plugin->supported = [500, 500];
