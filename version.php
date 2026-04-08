b36cdeb0e0a8d093d3df58b884627353c0017cc6
berthob98 <65233148+berthob98@users.noreply.github.com>
2026-04-08 17:39:39 +0200

    Split IDP logins and show Moodle-internal WAYF directly on the login page, resolves #1130 (#1206)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041454;
+$plugin->version = 2025041455;
 $plugin->release = 'v5.0-r22';
 $plugin->requires = 2025041401;
 $plugin->supported = [500, 500];
