d0e0b027452d1ef2c7a5b5fa4d10b4b1adcceb10
Alexander Bias <abias@ssystems.de>
2026-05-01 16:07:02 +0200

    Improvement: Add setting for background-size to advertisement tiles, resolves #1195 (#1250)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041458;
+$plugin->version = 2025041459;
 $plugin->release = 'v5.0-r23';
 $plugin->requires = 2025041401;
 $plugin->supported = [500, 500];
