af49fb4d6277654d6b9ceae4961562f18acbd63f
Alexander Bias <abias@ssystems.de>
2026-05-14 07:20:45 +0200

    Improvement: Allow the admin to further control the logo, heading, and tagline presentation on the login page, resolves #1262 (#1263)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union';
-$plugin->version = 2025041463;
+$plugin->version = 2025041464;
 $plugin->release = 'v5.0-r24';
 $plugin->requires = 2025041401;
 $plugin->supported = [500, 500];
