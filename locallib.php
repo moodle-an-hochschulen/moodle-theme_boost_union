5eb5f7285ce3857a83511a1495fa94dfff25f101
Alexander Bias <bias@alexanderbias.de>
2026-07-10 08:00:10 +0200

    Fix the usage of clean_string after backport of #1330

--- locallib.php
+++ locallib.php
@@ -2396,7 +2396,7 @@ function theme_boost_union_get_navbar_starredcoursespopover() {
         if ($course->visible || $canviewhiddencourses) {
             $coursesfortemplate[] = [
                 'url' => new \core\url('/course/view.php', ['id' => $course->id]),
-                'fullname' => clean_string(\core_external\util::format_string($course->fullname, $context, true, [])),
+                'fullname' => format_string($course->fullname, true, ['context' => $context, 'escape' => false]),
                 'visible' => $course->visible == 1,
             ];
         }
